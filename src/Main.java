fa7d29368eb4c961e53b53a55d7ab944aa4d97df
T.Atkins <43272999+tyronegit@users.noreply.github.com>
2024-04-13 15:04:48 -0400

    Modified with one code.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,39 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println (getDurationString(3945)); //This is the first test case
+        System.out.println (getDurationString ( 65,45 )); // This is the second test case
+    }
+
+    public static String getDurationString(int seconds) {
+
+        // Two-step approach to get hours
+        int minutes = seconds / 60;
+        return getDurationString ( minutes, seconds );
+ /*       int hours = minutes / 60;
+//        System.out.println ("hours = " + hours); // Used to verify that code is working
+
+        *//*one-step approach to get hours
+        int hours1 = seconds / 3600;
+        System.out.println ("hours1 = " + hours1);*//*
+        int remainingMinutes = minutes % 60;
+        *//* System.out.println ("minutes = " + minutes);
+        System.out.println ("remainingMinutes = " + remainingMinutes);*//*
+        int remainingSeconds = seconds % 60;
+//        System.out.println ("remainingSeconds = " + remainingSeconds);
+
+        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s ";*/
+
+    }
+
+    public static String getDurationString(int minutes,int seconds) {
+
+        int hours = minutes / 60;
+
+        int remainingMinutes = minutes % 60;
+        int remainingSeconds = seconds % 60;
+
+        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s ";
+    }
 }
