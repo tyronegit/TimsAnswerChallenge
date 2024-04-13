8f8de1a7fe8bbe67dfa0e36d1f6759ee929e41a1
T.Atkins <43272999+tyronegit@users.noreply.github.com>
2024-04-13 15:36:07 -0400

    Modified with one code.

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println (getDurationString(3945)); //This is the first test case
-        System.out.println (getDurationString ( 65,45 )); // This is the second test case
+        System.out.println (getDurationString(-3945)); //This is the first test case
+        System.out.println (getDurationString ( -65,-45)); // This is the second test case
+        System.out.println (getDurationString ( 65,145));
+        System.out.println (getDurationString ( 65,45));
+        System.out.println (getDurationString(3945));
     }
 
     public static String getDurationString(int seconds) {
 
-        // Two-step approach to get hours
-        int minutes = seconds / 60;
-        return getDurationString ( minutes, seconds );
- /*       int hours = minutes / 60;
-//        System.out.println ("hours = " + hours); // Used to verify that code is working
+        if(seconds < 0){
+            return "Invalid data for seconds(" + seconds
+                    + "), must be a positive iteger value";
+        }
 
-        *//*one-step approach to get hours
-        int hours1 = seconds / 3600;
-        System.out.println ("hours1 = " + hours1);*//*
-        int remainingMinutes = minutes % 60;
-        *//* System.out.println ("minutes = " + minutes);
-        System.out.println ("remainingMinutes = " + remainingMinutes);*//*
-        int remainingSeconds = seconds % 60;
-//        System.out.println ("remainingSeconds = " + remainingSeconds);
-
-        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s ";*/
-
+        return getDurationString (seconds / 60, seconds % 60);
     }
 
     public static String getDurationString(int minutes,int seconds) {
 
+        if(minutes< 0){
+            return "Invalid data for minutes(" + minutes
+                    + "), must be a positive iteger value";
+        }
+
+        if (seconds <= 0 || seconds >= 59) {
+            return  "Invalid data for seconds(" + seconds
+                    + "), must be between 0 and 59";
+        }
+
         int hours = minutes / 60;
 
         int remainingMinutes = minutes % 60;
-        int remainingSeconds = seconds % 60;
 
-        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s ";
+        return hours + "h " + remainingMinutes + "m " + seconds + "s ";
     }
 }
